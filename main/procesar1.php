e1173cb509b3e9b0b5ce2a3fa4b89d0b6f8fc840
eduardo <eduardoacastillo222@gmail.com>
2021-07-23 16:23:44 -0500

    1er commit

---
+++ main/procesar1.php
@@ -0,0 +1,11 @@
+<?php
+$Nombre_Completo = $_POST['Nombre_Completo'];
+$Email = $_POST['Email'];
+$Celular = $_POST['Numero_Celular'];
+$Mensaje = $_POST['Mensaje'];
+
+echo "Recibimos... <br>";
+echo "Nombre_Completo: ".$Nombre_Completo."<br>";
+echo "Email: ".$Email."<br>";
+echo "Celular: ".$Numero_Celular."<br>";
+echo "Mensaje: ".$Mensaje."<br>";
